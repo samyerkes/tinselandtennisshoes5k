a5f8fd99d424c40b573be09dcef4f3805bcae3f2
Sam Yerkes <samuelyerkes@gmail.com>
2015-11-04 11:23:20 -0500

    edited times for registration email

--- resources/views/email/create.blade.php
+++ resources/views/email/create.blade.php
@@ -7,7 +7,7 @@
 
 <h1>Tinsel and Tennis Shoes 5k</h1>
 <p>Thanks {{ $registration->fname }} for registering for the Tinsel and Tennis Shoes 5k benefitting <a href="http://changetheworldrva.org">Change the World RVA</a>.</p>
-<p>As a reminder the event is on December 12th at 8 a.m.</p>
+<p>As a reminder, the event is on December 12th, 2015. Registration will start at 8 a.m. and the run starts at 9 a.m.</p>
 <p>Don't forget to tell all your friends about this fun event. We're looking forward to seeing everyone there!</p>
 
 </body>
